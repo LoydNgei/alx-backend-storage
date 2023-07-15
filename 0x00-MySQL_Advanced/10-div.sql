cff71c70549ebea2fe1260985fa39d17b55dca79
loyd ngei <loydngei@gmail.com>
2023-07-15 13:01:45 +0300

    MySQL Advanced

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,17 @@
+-- Create a function SafeDiv that divides (and returns) the first by second number
+-- Or returns 0 if the second number is equal to 0
+
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMETER $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURN FLOAT DETERMINISTIC
+BEGIN
+    IF (b = 0)
+    THEN
+        RETURN (0);
+    ELSE
+        RETURN (a / b);
+    END IF;
+END
+$$
+DELIMITER ;
---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Write a script that creates a trigger  that resets 
+-- The attribute valid_email only when email has changed
+
+DELIMITER//
+
+CREATE TRIGGER reset_attribute BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END//
+
+DELIMITER;
