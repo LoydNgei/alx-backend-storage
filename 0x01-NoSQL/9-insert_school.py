4e89382b4218f849b864dbbc63e6831fb8e6834b
Loyd Ngei <loydngei@gmail.com>
2023-07-18 12:46:26 +0300

    Insert new

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Python function that inserts a new document in a collection based on kwargs"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Returns the new _id"""
+    results = mongo_collection.insert(kwargs)
+
+    return results.inserted_id
