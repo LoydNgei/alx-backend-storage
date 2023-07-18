de178ec4e91084db2f0dd2ee79296dd35022c183
Loyd Ngei <loydngei@gmail.com>
2023-07-18 13:12:30 +0300

    find specific

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" a Python function that returns the list of school having a specific topic"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Return lst of school"""
+    results = mongo_collection.find_one({"topic": topic})
+    return results
